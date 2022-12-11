17772627c1fa5a6f341ec286fa1c1d6891ef4d92
grantshandy <grantshandy@gmail.com>
2022-12-10 21:43:11 -0700

    refactor to single crate

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # wasmstation
 
 A work-in-progress runtime for [wasm4](https://github.com/aduros/wasm4).
-
-## Project Structure
-
- - `/Cargo.toml` - Repository is a workspace
- - `/wasmstation` - Main crate which is home to common traits, constants, and functions. Also reexports backends/renderers when features are set.
- - `/wasmstation-backend-*` - Backend implementations (wasmer, [wasm-micro-runtime](https://github.com/bytecodealliance/wasm-micro-runtime)?)
- - `/wasmstation-renderer-*` - Renderer implementations (sdl2, wgpu, [embedded-graphics](https://crates.io/crates/embedded-graphics)?)
- - `/cli` - The main way the library will be used, also a demo implementation.
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,24 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 
 use std::error::Error;
 
-/// wasm4 specific memory addresses and values.
+/// Wasm4 constants and addresses.
 pub mod wasm4;
-
-use wasm4::FRAMEBUFFER_SIZE;
 
 /// Common trait for game renderers.
 pub trait Renderer {
     fn render(
         &mut self,
-        framebuffer: [u8; FRAMEBUFFER_SIZE],
+        framebuffer: [u8; wasm4::FRAMEBUFFER_SIZE],
         palette: [u8; 16],
     ) -> Result<(), Box<dyn Error>>;
 }
 
-/// Common trait for webassembly runtime backends.
+/// Common trait for webassembly backends.
 pub trait Backend {
-    // callbacks
     fn call_update(&mut self);
     fn call_start(&mut self);
-
-    // I/O
-    fn read_screen(&self, framebuffer: &mut [u8; FRAMEBUFFER_SIZE], palette: &mut [u8; 16]);
+    fn read_screen(&self, framebuffer: &mut [u8; wasm4::FRAMEBUFFER_SIZE], palette: &mut [u8; 16]);
     fn set_gamepad(gamepad: u32);
     fn set_mouse(x: i16, y: i16, buttons: u8);
 }
