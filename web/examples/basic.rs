99b71699c9ee190d336b246a5cff95a815158da3
grantshandy <granthandy@proton.me>
2023-04-01 15:07:20 -0600

    start work on WebAssembly Instance in WebBackend

--- web/examples/basic.rs
+++ web/examples/basic.rs
@@ -1,8 +1,9 @@
 #![no_main]
+#![no_std]
 
 use wasmstation_web::{wasm_bindgen::prelude::*, launch_canvas};
 
 #[wasm_bindgen(start)]
-pub fn start() {
-    launch_canvas(include_bytes!("watris.wasm"), "canvas");
+pub async fn start() -> Result<(), JsValue> {
+    launch_canvas(include_bytes!("watris.wasm"), "canvas").await
 }
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,23 +1,33 @@
+#![no_std]
+
+use js_sys::{WebAssembly::{Instance, self}, Object, Reflect};
+use wasm_bindgen::{JsValue, JsCast};
+use wasm_bindgen_futures::JsFuture;
 use wasm_logger::Config;
 
 pub use wasm_bindgen;
 
-pub fn launch_canvas(bytes: &[u8], id: &str) {
+pub async fn launch_canvas(bytes: &[u8], id: &str) -> Result<(), JsValue> {
     wasm_logger::init(Config::default());
 
     log::info!("starting backend...");
-    let mut backend = WebBackend::from_bytes(bytes);
+    let mut backend = WebBackend::from_bytes(bytes).await?;
+
+    Ok(())
 }
 
 struct WebBackend {
-    // instance: Instance,
+    instance: Instance,
 }
 
 impl WebBackend {
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        Self {
-            
-        }
+    pub async fn from_bytes(bytes: &[u8]) -> Result<Self, JsValue> {
+        log::debug!("instantiating from wasm buffer");
+        let instantiate = JsFuture::from(WebAssembly::instantiate_buffer(bytes, &Object::new())).await?;
+        let instance: Instance = Reflect::get(&instantiate, &"instance".into())?.dyn_into()?;
+        
+        Ok(Self {
+            instance,
+        })
     }
-    
 }
